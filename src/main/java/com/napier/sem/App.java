cedfd6e1a96c472648ef24e39d71ef115ffcaaeb
CruzCGbz <cecilcruz@my.uopeople.edu>
2024-11-01 18:40:19 -0600

    Adding MongoDB Support to Our Application

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,38 @@
 package com.napier.sem;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        // Connect to MongoDB on the local system using port 27000
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27000");
+
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create a document to store
+        Document doc = new Document("name", "Kevin Sim")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2021")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
+
+        // Close the MongoClient instance
+        mongoClient.close();
     }
 }
